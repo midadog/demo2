1dc6ccbc0625ed7397772ec9402a197b56465b06
morris <morrs@uuu.com>
2022-05-13 16:26:46 +0800

    add restcontroller in spring applicatoin

--- src/main/java/com/uuu/demo/demo2/Demo2Application.java
+++ src/main/java/com/uuu/demo/demo2/Demo2Application.java
@@ -2,10 +2,19 @@ package com.uuu.demo.demo2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Demo2Application {
 
+//	@GetMapping(value = /hiyou)
+	@RequestMapping("/")
+	public String hiyou() {
+		return "hi you";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(Demo2Application.class, args);
 	}
